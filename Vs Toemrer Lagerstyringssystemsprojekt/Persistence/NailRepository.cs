49004cc4fd079e7dd24979a5985feec8f90ed0ac
dandydan112 <dan.t.nguyen123@gmail.com>
2024-04-24 11:54:41 +0200

    Added images

--- Vs Toemrer Lagerstyringssystemsprojekt/Persistence/NailRepository.cs
+++ Vs Toemrer Lagerstyringssystemsprojekt/Persistence/NailRepository.cs
@@ -1,56 +1,56 @@
-﻿using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Vs_Toemrer_Lagerstyringssystemsprojekt.Model;
+﻿//using Microsoft.Data.SqlClient;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vs_Toemrer_Lagerstyringssystemsprojekt.Model;
 
-namespace Vs_Toemrer_Lagerstyringssystemsprojekt.Persistence
-{
-    public class NailRepository : IRepository<Nail>
-    {
-        private List<Nail> Nail_Materials = new List<Nail>();
+//namespace Vs_Toemrer_Lagerstyringssystemsprojekt.Persistence
+//{
+//    public class NailRepository : IRepository<Nail>
+//    {
+//        private List<Nail> Nail_Materials = new List<Nail>();
 
-        public NailRepository()
-        {
-            InitializeRepository();
-        }
+//        public NailRepository()
+//        {
+//            InitializeRepository();
+//        }
 
 
-        private void InitializeRepository()
-        {
-            using (SqlConnection connection = new SqlConnection(RepositoryHelper.connectionString))
-            {
-                connection.Open();
+//        private void InitializeRepository()
+//        {
+//            using (SqlConnection connection = new SqlConnection(RepositoryHelper.connectionString))
+//            {
+//                connection.Open();
 
-                SqlCommand command = new SqlCommand("SELECT Length, Form, Quantity, Treatment FROM NAIL_MATERIAL", connection);
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        //SPØRG LEIF
-                        Nail nail = new Nail
-                        (
-                            Convert.ToDouble(reader["Length"].ToString()),
-                            reader["Form"].ToString(),
-                            int.Parse(reader["Quantity"].ToString()),
-                            reader["Treatment"].ToString()
-                        );
-                        Nail_Materials.Add(nail);
-                    }
-                }
-            }
-        }
-        public Nail Get(Nail t)
-        {
-            throw new NotImplementedException();
-        }
+//                SqlCommand command = new SqlCommand("SELECT Length, Form, Quantity, Treatment FROM NAIL_MATERIAL", connection);
+//                using (SqlDataReader reader = command.ExecuteReader())
+//                {
+//                    while (reader.Read())
+//                    {
+//                        //SPØRG LEIF
+//                        Nail nail = new Nail
+//                        (
+//                            Convert.ToDouble(reader["Length"].ToString()),
+//                            reader["Form"].ToString(),
+//                            int.Parse(reader["Quantity"].ToString()),
+//                            reader["Treatment"].ToString()
+//                        );
+//                        Nail_Materials.Add(nail);
+//                    }
+//                }
+//            }
+//        }
+//        public Nail Get(Nail t)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public List<Nail> GetAll()
-        {
-            return Nail_Materials;
-        }
+//        public List<Nail> GetAll()
+//        {
+//            return Nail_Materials;
+//        }
 
-    }
-}
+//    }
+//}
