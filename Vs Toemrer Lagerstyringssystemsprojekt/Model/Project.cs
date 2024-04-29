5d5e54bc1840c9281dde9f1197fc0d1a0c09fdd6
jakob <jakobzden1@gmail.com>
2024-04-29 10:28:47 +0200

    Properties done

---
+++ Vs Toemrer Lagerstyringssystemsprojekt/Model/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vs_Toemrer_Lagerstyringssystemsprojekt.Model
+{
+    public class Project
+    {
+        public string Title { get; set; }
+        public double Offer { get; set; }
+        public string Address { get; set; }
+        public string ProjectDescription { get; set; }
+
+    }
+}
