0be30eb267baa065f1f40743a120b41c5a098c48
jakob <jakobzden1@gmail.com>
2024-04-29 12:15:53 +0200

    Lidt fixes

---
+++ Vs Toemrer Lagerstyringssystemsprojekt/Viewmodel/ProjectViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vs_Toemrer_Lagerstyringssystemsprojekt.Viewmodel
+{
+    public class ProjectViewModel
+    {
+        public string Title { get; set; }
+        public double Offer { get; set; }
+        public string Address { get; set; }
+        public string ProjectDescription { get; set; }
+
+        public ProjectViewModel(string title, double offer, string address, string projectDescription)
+        {
+            Title = title;
+            Offer = offer;
+            Address = address;
+            ProjectDescription = projectDescription;
+        }
+
+    }
+}
