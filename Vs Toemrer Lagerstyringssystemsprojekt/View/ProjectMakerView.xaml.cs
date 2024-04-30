e9ddc7efa9fc684e65eb8c74c7c933392658d911
dandydan112 <dan.t.nguyen123@gmail.com>
2024-04-30 10:56:37 +0200

    Arbejdet på ProjectMakerView

---
+++ Vs Toemrer Lagerstyringssystemsprojekt/View/ProjectMakerView.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vs_Toemrer_Lagerstyringssystemsprojekt.View
+{
+    /// <summary>
+    /// Interaction logic for ProjectMakerView.xaml
+    /// </summary>
+    public partial class ProjectMakerView : UserControl
+    {
+        public ProjectMakerView()
+        {
+            InitializeComponent();
+        }
+
+        private void txbTitle_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == "Titel")
+            {
+                textBox.Text = ""; // Clear the placeholder text
+                textBox.TextAlignment = TextAlignment.Left; // Change text alignment to left
+            }
+        }
+
+        private void txbOffer_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == "Tilbud")
+            {
+                textBox.Text = ""; // Clear the placeholder text
+                textBox.TextAlignment = TextAlignment.Left; // Change text alignment to left
+            }
+        }
+
+        private void txbAddress_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == "Adresse")
+            {
+                textBox.Text = ""; // Clear the placeholder text
+                textBox.TextAlignment = TextAlignment.Left; // Change text alignment to left
+            }
+        }
+
+        private void txbDescription_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == "Projektbeskrivelse")
+            {
+                textBox.Text = ""; // Clear the placeholder text
+                textBox.TextAlignment = TextAlignment.Left; // Change text alignment to left
+            }
+        }
+    }
+}
