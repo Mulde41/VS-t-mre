d9de6d94c98ba99bd050d3f9fa0cfd5e7d44193c
Mulde41 <145011065+Mulde41@users.noreply.github.com>
2024-05-01 09:38:58 +0200

    WPF changes

--- Vs Toemrer Lagerstyringssystemsprojekt/View/ProjectsView.xaml.cs
+++ Vs Toemrer Lagerstyringssystemsprojekt/View/ProjectsView.xaml.cs
@@ -27,20 +27,14 @@ namespace Vs_Toemrer_Lagerstyringssystemsprojekt.View
         public ProjectsView()
         {
             InitializeComponent();
-            DataContext = new { Projects= ProjectsListBox };
+            DataContext = new { Projects = ProjectsListBox };
             LoadLists();
         }
         private void LoadLists()
         {
-            //    var materials = new List<Material>
-            //{
-            //    new Wood ("Fyrretræ", "Reglar", "Olieret", 20.0, 200.0, 25.0, 50),
-            //    new Wood ("Piktræ", "phallus", "Olieret", .0, 10.0, 3.0, 5000),
-            //    new Wood ("Fyrretræ", "Reglar", "Olieret", 20.0, 230.0, 25.0, 50),
-            //};
 
-        List<Project> projects = new List<Project> 
-        { 
+            List<Project> projects = new List<Project>
+        {
             new Project("Føtex tag", 5000.0, "Vestergade 12","der skal laves et tag på den nye føtex bygning på vestergade 12"),
             new Project("City Park Renovation", 20000.0, "Main Street Park", "This project involves a full renovation of Main Street Park, including landscaping, playground installation, and park facilities upgrades."),
             new Project("Library Expansion", 15000.0, "Central Library, Elm Street", "The Central Library is expanding with a new wing to house additional collections, reading rooms, and a community space.")
