ee450e12f95bb604a58400d6cd29ec4cb9c28e9f
jakob <jakobzden1@gmail.com>
2024-05-07 09:54:36 +0200

    MaterialViewModel done

---
+++ Vs Toemrer Lagerstyringssystemsprojekt/Viewmodel/MaterialViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vs_Toemrer_Lagerstyringssystemsprojekt.Model;
+
+namespace Vs_Toemrer_Lagerstyringssystemsprojekt.Viewmodel
+{
+    public class MaterialViewModel : INotifyPropertyChanged 
+    {
+        public int Quantity { get; set; }
+        public string Treatment { get; set; }
+        public string Name { get; set; }
+
+        public MaterialViewModel(Material material) 
+        {
+            Quantity = material.Quantity;
+            Treatment = material.Treatment;
+            Name = material.Name;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
