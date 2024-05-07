15a3a994717031b42303bcfc05ec87e1358e8a42
Mulde41 <145011065+Mulde41@users.noreply.github.com>
2024-05-07 10:52:08 +0200

    repo´sitory changes

---
+++ Vs Toemrer Lagerstyringssystemsprojekt/Viewmodel/MaterialViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vs_Toemrer_Lagerstyringssystemsprojekt.Model
+{
+    public abstract class MaterialViewModel : INotifyPropertyChanged
+    {
+        public int Quantity { get; set; }
+        public string Treatment { get; set; }
+        public abstract string Name { get; }
+
+        protected MaterialViewModel(int quantity, string treatment)
+        {
+            this.Treatment = treatment;
+            this.Quantity = quantity;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
