4c30fd932e98cf0637ca435b573f36f23ce43876
Mulde41 <145011065+Mulde41@users.noreply.github.com>
2024-05-08 10:49:55 +0200

    Service layer

---
+++ Vs Toemrer Lagerstyringssystemsprojekt/Viewmodel/MaterialSearchViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Vs_Toemrer_Lagerstyringssystemsprojekt.Business_Infrastructure;
+using Vs_Toemrer_Lagerstyringssystemsprojekt.Model;
+
+namespace Vs_Toemrer_Lagerstyringssystemsprojekt.Viewmodel
+{
+    public class MaterialSearchViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private MaterialSearchService _searchService;
+        public ObservableCollection<Material> Materials { get; private set; }
+        public ICommand SearchCommand { get; private set; }
+        private string _searchTerm;
+
+
+        public string SearchTerm
+        {
+            get => _searchTerm;
+            set
+            {
+                _searchTerm = value;
+                OnPropertyChanged(nameof(SearchTerm));
+            }
+        }
+
+        public MaterialSearchViewModel(MaterialSearchService searchService)
+        {
+            _searchService = searchService;
+            Materials = new ObservableCollection<Material>();
+            SearchCommand = new RelayCommand(PerformSearch);
+        }
+
+        private void PerformSearch()
+        {
+            Materials.Clear();
+            var results = _searchService.SearchMaterials(SearchTerm);
+            foreach (var material in results)
+            {
+                Materials.Add(material);
+            }
+        }
+    }
+}
