879037777cce285de0f0fab76381b20f1fe64294
MaxBensimon <f_mb@live.dk>
2024-05-20 11:21:34 +0200

    UnitTesting

--- UnitTester/UnitTest1.cs
+++ UnitTester/UnitTest1.cs
@@ -1,11 +1,43 @@
+using Vs_Toemrer_Lagerstyringssystemsprojekt.Business_Infrastructure;
+using Vs_Toemrer_Lagerstyringssystemsprojekt.Model;
+using Vs_Toemrer_Lagerstyringssystemsprojekt.Persistence;
+using Vs_Toemrer_Lagerstyringssystemsprojekt.Persistence.MaterialRepositories;
+
 namespace UnitTester
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private WoodRepository woodRepo;
+        private NailRepository nailRepo;
+        private ScrewRepository screwRepo;
+        private ProjectRepository projectRepo;
+        private MaterialSearchService<IMaterial> mss;
+
+        [TestInitialize]
+        public void Setup()
         {
+            woodRepo = WoodRepository.Instance;
+            nailRepo = NailRepository.Instance;
+            screwRepo = ScrewRepository.Instance;
+            projectRepo = ProjectRepository.Instance;
+            mss = new MaterialSearchService<IMaterial>(woodRepo, nailRepo, screwRepo);
+        }
+
+        [TestMethod]
+        public void MaterialSearchServiceReturnsBasedOnASearchTerm()
+        {
+            // This TestMethod is directly tied to the database, as the repository cannot be accessed without the database.
+
+            // Arrange
+            string searchTerm = "p";
+
+            // Act
+            var results = mss.SearchMaterials(searchTerm).Select(m => m.Name).ToList();
+
+            // Assert
+            var expectedResults = new List<string> { "Phillips 5mm Hardened", "Philips Head 10x0,5mm Greasy!" }; // This variable needs to change based on whatever is in the repositories...
+            CollectionAssert.AreEqual(expectedResults, results);
         }
     }
 }
